888c148b47a4a13d3ee7f91213c9bb695cb2575a
Julia Pereira <juliapereira.odc@gmail.com>
2024-03-23 18:17:53 -0300

    Trazer componente Select para o arquivo App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-import BasicSelect from "./components/Select"
+import React from "react"
+import { Box, FormControl, InputLabel, MenuItem } from '@mui/material';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 function App() {
+  const [year, setYear] = React.useState('');
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setYear(event.target.value as string);
+  };
 
   return (
     <>
@@ -8,7 +15,33 @@ function App() {
         react-campeonato-brasileiro
       </div>
       <div className="div-body">
-        <BasicSelect />
+        <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Ano</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={year}
+          label="Ano"
+          onChange={handleChange}
+        >
+          <MenuItem value={2003}>2003</MenuItem>
+          <MenuItem value={2004}>2004</MenuItem>
+          <MenuItem value={2005}>2005</MenuItem>
+          <MenuItem value={2006}>2006</MenuItem>
+          <MenuItem value={2007}>2007</MenuItem>
+          <MenuItem value={2008}>2008</MenuItem>
+          <MenuItem value={2009}>2009</MenuItem>
+          <MenuItem value={2010}>2010</MenuItem>
+          <MenuItem value={2011}>2011</MenuItem>
+          <MenuItem value={2012}>2012</MenuItem>
+          <MenuItem value={2013}>2013</MenuItem>
+          <MenuItem value={2014}>2014</MenuItem>
+          <MenuItem value={2015}>2015</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+        <span style={{ marginTop: '20px' }} ><strong>Campeonato Brasileiro de {year}</strong></span>
       </div>
     </>
   )
